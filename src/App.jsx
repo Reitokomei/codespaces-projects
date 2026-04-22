710434f649dc51e92657f2d9a40662508fea8a7f
Lê Đăng Quan Minh <200091093+Reitokomei@users.noreply.github.com>
2026-04-22 10:29:21 +0000

    dist

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Lê Đăng Quan Minh",
+  title: "Ai Engineer & Content Creator",
+  email: "mingquag@gmail.com",
+  gitHub: "github.com/reitokomei",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
